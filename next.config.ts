6b723686b4897ad793a8489781854fe43b130d37
sonyhell666 <dogcat1223@list.ru>
2026-03-13 18:27:56 +0300

    ignore build errors

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // Игнорируем ошибки TypeScript при сборке
     ignoreBuildErrors: true,
   },
   eslint: {
-    // Игнорируем ошибки линтера (правила оформления кода)
     ignoreDuringBuilds: true,
   },
 };
